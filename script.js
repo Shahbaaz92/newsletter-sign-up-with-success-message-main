663b43e09c960973dca4d94a469fbe8c055ec49f
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2024-02-19 17:38:11 +0530

    JS and css update

--- script.js
+++ script.js
@@ -7,16 +7,30 @@
 //         <!-- Success message end -->
 
 const emailBtn = document.getElementById("email-button");
+const emailAddress = document.getElementById("email-address");
 const mainCon = document.getElementById("main-container");
+const error = document.getElementById("error");
 
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 emailBtn.addEventListener("click", () => {
-  mainCon.classList.add("width");
-  mainCon.innerHTML = `
+  if (emailAddress.value === validateEmail(emailAddress.value)) {
+    mainCon.classList.add("width");
+    mainCon.innerHTML = `
   <section class="success-content">
 <img src="./assets/images/icon-success.svg" class="success" alt="success">
 <h1>Thanks for subscribing!</h1>
 <p> A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription.</p>
-<button>Dismiss message</button>
+<button  onClick="window.location.reload()">Dismiss message</button>
 </section>
 `;
+  } else {
+    emailAddress.classList.add("orange");
+    error.style.display = "block";
+  }
 });
